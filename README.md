d14b88d07b2756f7218d38fe2119268bf510baf2
Raxxius <raxxman@gmail.com>
2023-07-19 22:08:04 +0100

    update buttons styling

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 https://www.magicpattern.design/tools/css-backgrounds
+
+https://svg2jsx.com/
--- src/app/components/Intro.jsx
+++ src/app/components/Intro.jsx
@@ -1,3 +1,6 @@
+import Github from "../resources/github-icon";
+import Sheets from "../resources/google-sheets";
+
 export default function Intro() {
   return (
     <div className="intro primary">
@@ -6,16 +9,18 @@ export default function Intro() {
         This website is an active demo of Fetching Data From Google applications
       </p>
       <p className="text">For this test Google sheets is used</p>
-      <button
-        className="secondary"
-        href="https://docs.google.com/spreadsheets/d/1xAY6jBd5cJPIF3UfnsDveUZL-f_qNShGb3WFQItq-vw/edit#gid=0"
-        target="_blank"
-      >
-        View the Sheet
-      </button>
-      <button href="" className="secondary">
-        Github
-      </button>
+      <div className="button-box">
+        <button
+          className="secondary"
+          href="https://docs.google.com/spreadsheets/d/1xAY6jBd5cJPIF3UfnsDveUZL-f_qNShGb3WFQItq-vw/edit#gid=0"
+          target="_blank"
+        >
+          View Google Sheet <Sheets />
+        </button>
+        <button href="" className="secondary">
+          See the source code <Github />
+        </button>
+      </div>
     </div>
   );
 }
